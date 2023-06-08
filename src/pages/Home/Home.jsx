076d808f3d8d39750970c203a8b1ac31c20ee66e
FahimCC <ffnasif@gmail.com>
2023-06-08 15:40:24 +0600

    added a popular classes section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import useTitle from '../../hooks/useTitle';
+import PopularClasses from './PopularClasses';
 import Slider from './Slider';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
 	return (
 		<div>
 			<Slider />
+			<PopularClasses />
 		</div>
 	);
 };
